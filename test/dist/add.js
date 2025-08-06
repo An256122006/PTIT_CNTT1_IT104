6067385093b7c92ef7d88b10c6ae94d7f10a1ea4
trongan <dinhtrongan2006@gmail.com>
2025-08-06 22:04:39 +0700

    origin

--- test/dist/add.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mess = "hello anspider";
-alert(mess);
-var num = Number(prompt("moi ban nhap") || "0");
--- test/src/add.ts
+++
@@ -1,5 +0,0 @@
-let mess="hello anspider";
-alert(mess);
-
-let num: number = Number(prompt("moi ban nhap") || "0");
-
