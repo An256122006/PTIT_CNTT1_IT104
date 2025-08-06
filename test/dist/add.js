3111e6f44f84c93f82953b086ae2fe5222a54dbe
trongan <dinhtrongan2006@gmail.com>
2025-08-06 21:20:17 +0700

    bai tap

---
+++ test/dist/add.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mess = "hello anspider";
+alert(mess);
+var num = Number(prompt("moi ban nhap") || "0");
---
+++ test/src/add.ts
@@ -0,0 +1,5 @@
+let mess="hello anspider";
+alert(mess);
+
+let num: number = Number(prompt("moi ban nhap") || "0");
+
